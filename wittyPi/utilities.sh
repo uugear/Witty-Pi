551e03344fbe8f0af646ac8de74bbc119fd9e46e
Shawn <shawn@uugear.com>
2016-07-11 11:41:24 +0200

    V2.183: fix a bug that scheduling a time at first day of the month

--- wittyPi/utilities.sh
+++ wittyPi/utilities.sh
@@ -166,7 +166,13 @@ get_local_date_time()
   if [ $second == '??' ]; then
     second='00'
   fi
-  local datestr=$(date +%Y-%m-)
+  local datestr=$(date +%Y-)
+  local curDate=$(date +%d)
+  if [[ "$date" < "$curDate" ]] ; then
+    datestr+=$(date --date="$(date +%m) +1 month" +%m-)
+  else
+    datestr+=$(date +%m-)
+  fi
   datestr+="$date $hour:$minute:$second UTC"
   local result=$(date -d "$datestr" +"%d %H:%M:%S" 2>/dev/null)
   IFS=' ' read -r date timestr <<< "$result"
