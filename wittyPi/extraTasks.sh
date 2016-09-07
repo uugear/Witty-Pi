892f3562d3b4abbbb0b60494f15e181daf3c4384
comperta <dineaus@gmail.com>
2016-09-07 18:31:24 +0200

    Create extraTasks.sh

---
+++ wittyPi/extraTasks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# file: extraTasks.sh
+#
+# This script will be launched in background after Witty Pi 2 get initialized.
+# If you want to run your commands after boot, you can place them here.
+#
